8c562f2fb1a23e88a73b9bf7dcac6bfd8c231979
sfringer-umich <80358770+sfringer-umich@users.noreply.github.com>
2022-01-24 13:52:06 -0500

    Add files via upload

---
+++ ps2-my-code.js
@@ -0,0 +1,84 @@
+/**
+ * SI 579: Problem Set 2 - JavaScript Tests
+ * 
+ * Do at least three of the four following problems. Load "ps2-tests.html" in your browser to view the results.
+ */
+
+/**
+ * Define `addOne` as a function that expects one argument (a number)
+ * and returns that argument's value plus one.
+ */
+
+function addOne(x) {
+  x += 1
+  return(x)
+}
+
+/**
+ * Define `largest` as a function that expects one argument (an array of numbers)
+ * and returns the largest item in that array.
+ */
+
+function largest(lst) {
+  return(Math.max.apply(Math, lst))
+}
+
+/**
+ * The *factorial* of a number n is denoted as: n! === n * (n-1) * (n-2) * ... * 1
+ * 
+ * For example, 4! === 4 * 3 * 2 * 1 === 24
+ * Another example: 6! === 6 * 5 * 4 * 3 * 2 * 1 === 720
+ * 
+ * Define a function `fact` that accepts one argument (a positive integer n) and returns n!
+ * 
+ * Note: 0! is 1
+ */
+
+function fact(n) {
+  if (n==0 | n==1) {
+    return(1)
+  }
+
+  for (var x = n - 1; x >= 1; x--) {
+    n = n * x;
+  }
+
+  return(n)
+}
+
+/**
+  * A Fibonacci sequence is the integer sequence 0, 1, 1, 2, 3, 5, 8...
+  * the first two numbers are 0 and 1 and every number thereafter is the sum
+  * of the previous two numbers. Write a function named `fib` that accepts
+  * one argument (a number n) and returns the n-th element of the Fibonacci sequence.
+  * 
+  * You should assume that n is zero-indexed (meaning fib(0) == 0).
+  * Example calls:
+  * 
+  * fib(0) === 0
+  * fib(1) === 1
+  * fib(2) === 1
+  * fib(3) === 2
+  * fib(4) === 3
+  * fib(5) === 5
+  * fib(6) === 8
+  * ...
+  * 
+  * You can assume that the argument, n, is a positive integer. Do not worry about efficiency.
+  */
+
+function fib(n) {
+  let n_0 = 0;
+  let n_1 = 1;
+  let n_next;
+
+  while (n > 0) {
+    n_next = n_1;
+    n_1 = n_0 + n_1;
+    n_0 = n_next;
+    n--;
+  }
+
+  return(n_0);
+}
+
